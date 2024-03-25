4f5a3c09484538cb965654560015ed7ae232903e
speady1445 <sykora1445@gmail.com>
2024-03-25 14:42:10 +0100

    Add extraction of URLs from HTML

--- crawl.js
+++ crawl.js
@@ -9,6 +9,21 @@ function normalizeURL(str) {
     return normalizedUrl;
 }
 
+function getURLsFromHTML(htmlBody, baseURL) {
+    const dom = new JSDOM(htmlBody);
+    const aElements = dom.window.document.querySelectorAll('a');
+    const links = [];
+    for (const aElement of aElements) {
+        if (aElement.href.slice(0, 1) === "/") {
+            links.push(`${baseURL}${aElement.href}`);
+        } else {
+            links.push(aElement.href);
+        }
+    }
+    return links;
+}
+
 module.exports = {
-    normalizeURL
+    normalizeURL,
+    getURLsFromHTML
 }
--- crawl.test.js
+++ crawl.test.js
@@ -1,5 +1,5 @@
 const { test, expect } = require('@jest/globals')
-const { normalizeURL } = require('./crawl.js')
+const { normalizeURL, getURLsFromHTML } = require('./crawl.js')
 
 
 test('normalizeURL', () => {
@@ -16,4 +16,35 @@ test('normalizeURL capitals', () => {
 
 test('normalizeURL http', () => {
     expect(normalizeURL('http://blog.boot.dev/path')).toEqual('blog.boot.dev/path');
+});
+
+const baseURL = 'https://blog.boot.dev';
+
+test('getURLsFromHTML no link', () => {
+    const htmlBody = '<html><body><p>Hello World</p></body></html>'
+    expect(getURLsFromHTML(htmlBody, baseURL)).toEqual([]);
+});
+
+test('getURLsFromHTML absolute', () => {
+    const html = `<html><body><a href="https://blog.boot.dev/path/smt">Link 1</a></body></html>`;
+    expect(getURLsFromHTML(html, baseURL)).toEqual(['https://blog.boot.dev/path/smt']);
+});
+
+test('getURLsFromHTML relative', () => {
+    const html = `<html><body><a href="/path/smt">Link 1</a></body></html>`;
+    expect(getURLsFromHTML(html, baseURL)).toEqual(['https://blog.boot.dev/path/smt']);
+});
+
+test('getURLsFromHTML both', () => {
+    const html = `
+        <html>
+            <body>
+                <a href="https://blog.boot.dev/path">Link 1</a>
+                <a href="/path2/something">Link 2</a>
+            </body>
+        </html>
+    `;
+    expect(getURLsFromHTML(html, baseURL)).toEqual(
+        ['https://blog.boot.dev/path', 'https://blog.boot.dev/path2/something']
+    );
 });
